13a7fc0025885b7950c6ecb2948c4ab9cbc1ca1b
2ao1-1 <ahmedomran1950@gmail.com>
2025-02-02 07:23:15 +0200

    edit get user data

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const User = require('../models/User');
-const verifyToken = require('../middlewares/auth');
+const express = require("express");
+const User = require("../models/User");
+const verifyToken = require("../middlewares/auth");
 
 const router = express.Router();
 
@@ -10,6 +10,45 @@ const router = express.Router();
  *   name: Users
  *   description: User management routes
  */
+
+/**
+ * @swagger
+ * /api/users/profile:
+ *   get:
+ *     summary: Get current user profile
+ *     tags: [Users]
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Current user profile data
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ *                 firstName:
+ *                   type: string
+ *                 lastName:
+ *                   type: string
+ *       401:
+ *         description: Not authenticated
+ */
+router.get("/profile", verifyToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 /**
  * @swagger
@@ -38,14 +77,14 @@ const router = express.Router();
  *                   lastName:
  *                     type: string
  */
-router.get('/', verifyToken, async (req, res) => {
+router.get("/", verifyToken, async (req, res) => {
   try {
     const users = await User.find({ _id: { $ne: req.user.id } }).select(
-      '-password'
+      "-password"
     );
     res.json(users);
   } catch (error) {
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: "Server error" });
   }
 });
 
